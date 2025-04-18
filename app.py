996fe2191b0ba3706825e919b158942528d97928
Colton Younger <coltonyounger@gmail.com>
2025-04-18 13:02:34 -0400

    Initial commit with Flask backend

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # For local development
+
+app = Flask(__name__)
+
+# Database connection
+def get_db_connection():
+    conn = psycopg2.connect(os.environ['postgresql://hds_main_database_user:MDOpEbGNqP5uERqaEBZlbZgrNW9gM6TU@dpg-d00i93c9c44c73fn053g-a.virginia-postgres.render.com/hds_main_database'],
+                           sslmode='require')
+    return conn
+
+# Test endpoint
+@app.route('/')
+def hello():
+    return 'Hello from Flask!'
+
+# Get all customers
+@app.route('/api/customers')
+def get_customers():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM customer_data;')
+    customers = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(customers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
