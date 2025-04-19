6e69f11db712d69e57c0db4bacaeae42a68dc916
Colton Younger <coltonyounger@gmail.com>
2025-04-19 18:28:15 -0400

    Updated to add additional tables from the database

--- app.py
+++ app.py
@@ -44,5 +44,60 @@ def get_inventory():
     conn.close()
     return jsonify(inventory)
 
+# Get employee data
+@app.route('/api/employees')
+def get_employees():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM employee_data;')
+    employees = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(employees)
+
+# Get current jobs
+@app.route('/api/current_jobs')
+def get_current_jobs():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM curent_jobs;')
+    current_jobs = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(current_jobs)
+
+# Get orders
+@app.route('/api/orders')
+def get_orders():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM orders;')
+    orders = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(orders)
+
+# Get pending jobs
+@app.route('/api/pending_jobs')
+def get_pending_jobs():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM pending_jobs;')
+    pending_jobs = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(pending_jobs)
+
+# Get sales records
+@app.route('/api/sales_records')
+def get_sales_records():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM sales_records;')
+    sales_records = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(sales_records)
+    
 if __name__ == '__main__':
     app.run(debug=True)
